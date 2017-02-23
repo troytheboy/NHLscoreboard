5b130fffc02ab0dd5436af67ff26bd955fb4b4bc
troytheboy <tbarn2@uw.edu>
2017-02-23 11:27:29 -0800

    working game start time

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-require('http')
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var htmlparser = require("htmlparser");
 var $ = require('jquery')
 var url = "http://statsapi.web.nhl.com/api/v1/game/2016020891/feed/live.json";
 
@@ -18,35 +16,39 @@ function getScore(gameId)
     var liveData = JSON.parse(JSON.stringify(result.liveData));
     var dateTime = gameData.datetime.dateTime;
     var time = new Date(dateTime);
-    //console.log(time > new Date());
-
     var away = gameData.teams.away.name;
     var home = gameData.teams.home.name;
     var boxscore = liveData.boxscore;
     var awayScore = boxscore.teams.away.teamStats.teamSkaterStats.goals;
     var homeScore = boxscore.teams.home.teamStats.teamSkaterStats.goals;
     if (time > new Date()) {
-      console.log(away + " @ " + home + " Game Start: " + time + "\n");
+      time = time.toISOString().split("T")[1];
+      var hours = time.substring(0,2);
+      hours = parseInt(hours, 10) + 7;
+      time = hours + time.substring(2,5) + " EST";
+      console.log(away + " @ " + home + " " + time + "\n");
     } else {
       console.log(away + " " + awayScore + " || " + home + " " + homeScore + "\n");
     }
+    return result;
 }
 
-function getGames() {
-    var date = new Date();
-    var yesterdayDate = date.setDate(date.getDate() -1);
+function getGames(date) {
+    //var date = new Date();
+    //var yesterdayDate = date.setDate(date.getDate() -1);
     //console.log(date,yesterdayDate)
-    var today = (new Date()).toISOString().split("T")[0];
-    var yesterday = (new Date(yesterdayDate)).toISOString().split("T")[0];
+    var today = date.toISOString().split("T")[0];
+    //var yesterday = (new Date(yesterdayDate)).toISOString().split("T")[0];
     var schedule = "https://statsapi.web.nhl.com/api/v1/schedule?startDate="+today+"&endDate="+today;
-    var scheduleYesterday = "https://statsapi.web.nhl.com/api/v1/schedule?startDate="+yesterday+"&endDate="+yesterday;
+    console.log(today);
+    //var scheduleYesterday = "https://statsapi.web.nhl.com/api/v1/schedule?startDate="+yesterday+"&endDate="+yesterday;
     //console.log(scheduleYesterday);
     var gameIds = parseGames(schedule);
-    var yesterdayIds = parseGames(scheduleYesterday);
-    console.log("Yesterday's Scores:");
-    yesterdayIds.forEach(function(element) {
-      getScore(element)
-    })    //console.log(gameIds);
+    //var yesterdayIds = parseGames(scheduleYesterday);
+    // console.log("Yesterday's Scores:");
+    // yesterdayIds.forEach(function(element) {
+    //   getScore(element)
+    // })    //console.log(gameIds);
 
     return gameIds;
 
@@ -65,10 +67,15 @@ function parseGames(schedule) {
   return gameIds;
 }
 
-//console.log(getScore(url));
-//console.log(getGames());
-var games = getGames();
-console.log("Todays Games: ");
-games.forEach(function(element) {
-  getScore(element)
-})
+function scoreboard(date) {
+  var games = getGames(date);
+  var gameData = [];
+  games.forEach(function(element) {
+    gameData.push(getScore(element));
+  });
+}
+
+var date = new Date;
+scoreboard(date);
+// var yesterday = new Date(date.setDate(date.getDate() -1));
+// scoreboard(yesterday);
